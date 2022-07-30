a94d7ab1b5affcb9969fd4c29fbbc6a2dbd57b10
Nawid Shadab <shadab.nawid@yahoo.com>
2022-07-30 03:16:06 +0200

    creating postgress service container_ removed model.Base from main

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ from .routers import post, user, auth, vote
 from .config import settings
 
 # as far as we now use alembic to create and updates our tables we can commnet this 
-models.Base.metadata.create_all(bind=engine)
+#models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
